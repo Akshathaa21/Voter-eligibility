3306df7d19031b22a8084cb234cec6450179d8cf
Akshathaa21 <165242510+Akshathaa21@users.noreply.github.com>
2024-09-25 13:26:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+{/* <script type="text/javascript">
+var age = parseFloat(prompt(Enter the age ))
+if ("age  18")
+document.write("Eligible to vote")
+else
+document.write("Not eligible")
+</script> */}
